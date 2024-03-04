58a947c01350fd25cab89aa1d699004967a8253d
mauricio.costa@yduqs.com.br <mauricio.costa@yduqs.com.br>
2024-03-04 19:13:50 -0300

    faltou subir um usecase

---
+++ src/usecase/listaAutores.ts
@@ -0,0 +1,11 @@
+import { AutorRepository } from "../infra/repositories/autor/autorRepostiory";
+import { Autor } from "../model/autor";
+export function listaAutores() {
+    const autorRepository = new AutorRepository();
+    const autores: any[] = autorRepository.getall();
+    if (autores != undefined) {
+        autores.map((e, i ,a) => {
+            console.log(e.nome, e.dataCadastro);
+        });
+    }
+}
